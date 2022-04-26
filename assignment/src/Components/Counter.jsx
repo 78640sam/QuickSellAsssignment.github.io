6bc9a778d6781c4c8fece0dbdd67ffd2c26d1be6
Shubham Mishra <shubhammishra78640@gmail.com>
2022-04-26 15:05:51 +0530

    updated

--- assignment/src/Components/Counter.jsx
+++ assignment/src/Components/Counter.jsx
@@ -2,14 +2,42 @@
  import "./counter.css";
 import {useState} from "react";
  function Counter(){
-     const[number,setnumber]=useState(10);
+     const[number,setNumber]=useState(10);
+    
+     if(number>1000){
+    
+      return  <>
+
+        <div className="main-counter">
+            <button className="counter-button-1" >-</button>
+            <input className="counter-input" type="text" value="1000"  />
+            <button className="counter-button-2">+</button>
+        </div>
+        </>   
+    }
+     const increment = ()=> {
+         setNumber(number+1)
+         console.log(number)
+         
+     }
+ 
+     const decrement = ()=> {
+        setNumber(number-1)
+        console.log(number)
+    } 
+ const showinput=(el)=>{
+   
+    setNumber((number)=> Number(number)+Number(el.target.value));
+    
+ }
+   
 
     return <>
 
     <div className="main-counter">
-        <button className="counter-button">-</button>
-        <input className="counter-input" type="number" />
-        <button className="counter-button">+</button>
+        <button className="counter-button-1" onClick={decrement}>-</button>
+        <input className="counter-input" type="text" value={number} onChange={showinput} />
+        <button className="counter-button-2" onClick={increment}>+</button>
     </div>
     </>
  }
